4725ba2f630f22fedc8cf35fa90aa803cb9a0a49
mira4sol <ahmadmuhammadmak5@gmail.com>
2025-12-08 12:04:50 +0100

    fix: updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
 import NotFound from "@/pages/not-found";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/next";
 import { useEffect } from "react";
+import { Route, Switch } from "wouter";
+import { queryClient } from "./lib/queryClient";
 
 function Router() {
   return (
@@ -27,6 +28,7 @@ function App() {
         <Toaster />
         <Router />
       </TooltipProvider>
+      <Analytics />
     </QueryClientProvider>
   );
 }
